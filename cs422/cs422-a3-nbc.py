8b82ab122ac1d8af7a28cdd5ed0e058d5da56b26
Michelle McGowan <mcgowm1@unlv.nevada.edu>
2023-10-24 10:33:41 -0700

    Score Labeling

--- cs422/cs422-a3-nbc.py
+++ cs422/cs422-a3-nbc.py
@@ -8,7 +8,7 @@ data = pd.read_csv("nba-players.csv")
 
 # get rid of unneeded stats about passenger 
 dataNew = data.drop(['name', 'id'], axis=1)
-print(dataNew)
+#print(dataNew)
 
 # what we what to know is if they are gonna last in the league 
 # for 5 years 
@@ -16,17 +16,24 @@ target = dataNew.target_5yrs
 
 # make input feature list without the Survived
 inputFeatures = dataNew.drop('target_5yrs', axis='columns')
+#print(inputFeatures)
 
-print(inputFeatures)
-
+# split the data
 X_train, X_test, y_train, y_test = train_test_split(inputFeatures, target, test_size=0.2)
 
-# #print(inputFeatures)
-
+# Gaussian Naive Bayes classifier
 model = GaussianNB()
 model.fit(X_train, y_train)
 
+# for training data
+print("Score for Train Data: ")
+print(model.score(X_train, y_train))
+#print(model.predict(X_train, y_train))
+
+# for test data
+print("Score for Test Data: ")
 print(model.score(X_test, y_test))
+print(model.predict(X_test))
 
 # print(y_test[:10])
 # print(model.predict(X_test[:10]))
